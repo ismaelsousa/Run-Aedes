8009566e24cd0479ac59ead118434a710d69169f
ismael sousa <ismaelsousa1998@outlook.com>
2021-01-23 15:42:27 -0300

    add question yes no

--- RunDengue/Assets/PauseQuestion.cs
+++ RunDengue/Assets/PauseQuestion.cs
@@ -6,9 +6,12 @@ public class PauseQuestion : MonoBehaviour
 {
 
     public GameObject question;
+    public GameObject score;
     public GameObject noQuestionButton;
     public GameObject yesQuestionButton;
     public GameObject butaoPause;
+    public GameObject[] questions;
+    public int selectedQuestion = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -22,32 +25,46 @@ public class PauseQuestion : MonoBehaviour
         
     }
 
+    public void checkAnswer(string answer){
+        bool correct = questions[selectedQuestion].GetComponent<QuestionAnswer>().isTrue;
+        if (answer == "yes") {
+            if (correct) {
+                Debug.Log("ACertou");
+                return;
+                score.SetActive(true);
+                butaoPause.SetActive(true);
+                question.SetActive(false);
+                noQuestionButton.SetActive(false);
+                yesQuestionButton.SetActive(false);
+
+                PauseMenu.GameIsPaused = false;
+                Time.timeScale = PauseMenu.velocidadeJogo;
+            };
+
+        } else if (!correct) {
+            Debug.Log("ACertou");
+            return;
+        };
+
+        Debug.Log("Errou");
+    }
+
     public void yes() {
         Debug.Log("Press yes");
-        butaoPause.SetActive(true);
-        question.SetActive(false);
-        noQuestionButton.SetActive(false);
-        yesQuestionButton.SetActive(false);
-
-        PauseMenu.GameIsPaused = false;
-        Time.timeScale = PauseMenu.velocidadeJogo;
+        checkAnswer("yes");      
     }
     public void no() {
         Debug.Log("Press no");
-        Time.timeScale = PauseMenu.velocidadeJogo;
-        butaoPause.SetActive(true);
-        question.SetActive(false);
-        noQuestionButton.SetActive(false);
-        yesQuestionButton.SetActive(false);
-
-        PauseMenu.GameIsPaused = false;
-        Time.timeScale = PauseMenu.velocidadeJogo;
+        checkAnswer("no");
+       
     }
+
     public void showQuestion()
     {
         Debug.Log("AQUIIIIII");
         butaoPause.SetActive(false);
         question.SetActive(true);
+        score.SetActive(false);
         noQuestionButton.SetActive(true);
         yesQuestionButton.SetActive(true);
 
---
+++ RunDengue/Assets/QuestionAnswer.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionAnswer : MonoBehaviour
+{
+    public bool isTrue;
+}
