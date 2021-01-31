9f31420b7c4bccf86f6d2d1517e7762aef887a73
ismael <ismaelsousa1998@outlook.com>
2021-01-31 13:30:58 -0300

    show random questions and destroy when is true

--- RunDengue/Assets/PauseQuestion.cs
+++ RunDengue/Assets/PauseQuestion.cs
@@ -6,12 +6,14 @@ public class PauseQuestion : MonoBehaviour
 {
 
     public GameObject question;
+    public GameObject lastQuestion;
+    public GameObject localToSpawnQuestions;
     public GameObject score;
     public GameObject noQuestionButton;
     public GameObject yesQuestionButton;
     public GameObject butaoPause;
-    public GameObject[] questions;
-    public int selectedQuestion = 0;
+    public List<GameObject> questions;
+    public int selectedQuestion = -1;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +21,13 @@ public class PauseQuestion : MonoBehaviour
         
     }
 
-   void showItems()
+    private void Update()
+    {
+        Debug.ClearDeveloperConsole();
+       // Debug.Log(Random.Range(0, 2));
+    }
+
+    void showItems()
     {
         score.SetActive(true);
         butaoPause.SetActive(true);
@@ -28,15 +36,34 @@ public class PauseQuestion : MonoBehaviour
         yesQuestionButton.SetActive(false);
     }
 
+    void cleanQuestion(bool delete)
+    {
+        if (delete)
+        {
+            Destroy(lastQuestion);
+            questions.Remove(questions[selectedQuestion]);
+            selectedQuestion = -1;
+        }
+        else
+        {
+            Destroy(lastQuestion);
+            selectedQuestion = -1;
+        }
+       
+
+    }
     public void checkAnswer(string answer){
+
+        // clean lastQuestion after check responses
+
         bool correct = questions[selectedQuestion].GetComponent<QuestionAnswer>().isTrue;
         if (answer == "yes") {
             if (correct) {
                 Debug.Log("ACertou");
-                score.SetActive(true);
                 showItems();
                 PauseMenu.GameIsPaused = false;
                 Time.timeScale = PauseMenu.velocidadeJogo;
+                cleanQuestion(true);
                 return;
 
             };
@@ -46,6 +73,7 @@ public class PauseQuestion : MonoBehaviour
             showItems();
             PauseMenu.GameIsPaused = false;
             Time.timeScale = PauseMenu.velocidadeJogo;
+            cleanQuestion(true);
             return;
         };
         showItems();
@@ -53,7 +81,7 @@ public class PauseQuestion : MonoBehaviour
         PauseMenu.velocidadeJogo += 0.05f;
         Time.timeScale = PauseMenu.velocidadeJogo;
         Debug.Log("Errou");
-
+        cleanQuestion(false);
     }
 
     public void yes() {
@@ -68,11 +96,28 @@ public class PauseQuestion : MonoBehaviour
 
     public void showQuestion()
     {
-        Debug.Log("AQUIIIIII");
+        if (questions.Count == 0) return;
+        // Generate a random number between 0 and legth - 1
 
+        selectedQuestion = Mathf.FloorToInt(Random.Range(0, questions.Count - 1));
+        Debug.Log("random "+ selectedQuestion.ToString());
+
+
+        // Create a new question
+        lastQuestion = Instantiate(questions[selectedQuestion]) as GameObject;
+
+        // set the position on the new object
+        lastQuestion.transform.position =
+            new Vector3(
+                localToSpawnQuestions.transform.position.x,
+                localToSpawnQuestions.transform.position.y,
+                localToSpawnQuestions.transform.position.z);
+
+        // put like son
+        lastQuestion.transform.SetParent(localToSpawnQuestions.transform);
 
         butaoPause.SetActive(false);
-        question.SetActive(true);
+       // question.SetActive(true);
         score.SetActive(false);
         noQuestionButton.SetActive(true);
         yesQuestionButton.SetActive(true);
